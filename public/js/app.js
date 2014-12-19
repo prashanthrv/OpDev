fec2300f3b40a9ff4bad59fdb3a52357f962814e
unknown <gameboxin@gmail.com>
2014-12-19 18:36:46 +0530

    Project Details route added.
    Mockup project details page added.

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,46 @@ OpDev = Ember.Application.create();
 OpDev.Router.map(function() {
   // put your routes here
   this.route('/');
-  this.route('projects');
+  this.resource('projects', function(){
+  	this.route('index');
+  	this.route('project', { path: "/:project_id" });
+  });
   this.route('sources');
   this.route('settings');
   this.route('about');
 });
 
+//Routes
 OpDev.IndexRoute = Ember.Route.extend({
   model: function() {
     return ['red', 'yellow', 'blue'];
   }
+});
+OpDev.ProjectsRoute = Ember.Route.extend({
+	model: function(){
+		var tempObj = {'projects':[{'id':1,'project_id':3,'name':'Caesars'},{'id':2,'project_id':4,'name':'11500_issues'}]};
+		return tempObj;
+	},
+});
+OpDev.ProjectsIndexRoute = Ember.Route.extend({
+	model: function(){
+		return null;
+	},
+	renderTemplate: function() {this.render({into: 'projects', outlet: 'projectsubpage'});}//No I18N
+});
+
+OpDev.ProjectsProjectRoute = Ember.Route.extend({
+	model: function(){
+		return null;
+	},
+	renderTemplate: function() {this.render({into: 'projects', outlet: 'projectsubpage'});}//No I18N
+});
+
+//Views
+OpDev.ProjectsIndexView = Ember.View.extend({
+	templateName:'addnewproject'
+});
+
+OpDev.ProjectsProjectView = Ember.View.extend({
+	templateName:'projectdetails'
 });
